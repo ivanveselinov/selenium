e3ff95ef98f1d7a6b6da563ddddcc9198502d1e9
Ivan <ivanveselinov22@gmail.com>
2024-01-29 10:21:53 +1100

    Xpath and Css exercise

--- exercise2/exercise1.py
+++ exercise2/exercise1.py
@@ -1,2 +1,28 @@
+from selenium import webdriver
+import time
+
+browser = webdriver.Chrome()
+
+browser.get("https://techstepacademy.com/training-ground")
+
+# Find elements ID or identifications
 input2_css_locator = "input[id='ipt2']"
-button4_xpath_locator = "//button[@id='b4'])"
+button4_xpath_locator = "//button[@id='b4']"
+
+# Assign elements
+input1_elem = browser.find_element("css selector", input2_css_locator)
+butn4_elem = browser.find_element("xpath", button4_xpath_locator)
+
+# Manipulate elements
+input1_elem.send_keys("Test text")
+
+time.sleep(5)
+
+butn4_elem.click()
+
+time.sleep(2)
+
+browser.quit()
+
+
+
